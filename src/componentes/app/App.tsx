7d92fe4ca475be527190ddfefe10ab271e0ea166
chico <chico.zimcolor@gmail.com>
2022-10-01 17:04:46 -0300

    app é flex

--- src/componentes/app/App.tsx
+++ src/componentes/app/App.tsx
@@ -1,10 +1,11 @@
+import "./App.css";
 import React from "react";
 import { PainelDeControle } from "../painelDeControle";
 import Tabela from "../tabela/Tabela";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <PainelDeControle />
       <Tabela />
     </div>
