57c742831d9a43907deb2455be3b49260fa95fef
fr-mm <chico.zimcolor@gmail.com>
2022-10-30 08:50:08 -0300

    setas ordenadoras mais genéricas

--- src/componentes/tabela/disciplina/Disciplina.tsx
+++ src/componentes/tabela/disciplina/Disciplina.tsx
@@ -1,17 +1,15 @@
 import "./Disciplina.css";
-import { useDispatch, useSelector } from "react-redux";
-import { EnumOrdem } from "../../../enums";
+import { useSelector } from "react-redux";
 import { ITabela } from "../../../interfaces";
 import { reducers, RootState } from "../../../store";
 import Competencia from "../competencia";
+import { SetasOrdenadoras } from "../setas";
 
 interface DisciplinaProps {
   idDisciplina: ITabela.IdDisciplina;
 }
 
 function Disciplina(props: DisciplinaProps) {
-  const dispatch = useDispatch();
-  const ordenacao = useSelector((state: RootState) => state.ordenacao);
   const docentesFiltrados = useSelector(
     (state: RootState) => state.docentes.filtrados
   );
@@ -19,64 +17,19 @@ function Disciplina(props: DisciplinaProps) {
     (state: RootState) => state.disciplinas.todas[props.idDisciplina]
   );
 
-  function mudarOrdem() {
-    if (ordenacao.idElemento === disciplina.id) {
-      switch (ordenacao.proximaOrdem) {
-        case EnumOrdem.DECRESCENTE:
-          dispatch(
-            reducers.docentes.ordenarPorCompetenciaDecrescente(disciplina.id)
-          );
-          break;
-        case EnumOrdem.CRESCENTE:
-          dispatch(
-            reducers.docentes.ordenarPorCompetenciaCrescente(disciplina.id)
-          );
-          break;
-        case EnumOrdem.NENHUMA:
-          dispatch(reducers.docentes.ordenarAlfabeticamente());
-      }
-      dispatch(reducers.ordenacao.alternarOrdem());
-    } else {
-      dispatch(
-        reducers.docentes.ordenarPorCompetenciaDecrescente(disciplina.id)
-      );
-      dispatch(reducers.ordenacao.mudarElemento(disciplina.id));
-    }
-  }
-
-  function setasOrdenadoras() {
-    const corSeta = {
-      esquerda: "cinza",
-      direita: "cinza",
-    };
-
-    if (ordenacao.idElemento === disciplina.id) {
-      switch (ordenacao.ordem) {
-        case EnumOrdem.CRESCENTE:
-          corSeta.esquerda = "preta";
-          break;
-        case EnumOrdem.DECRESCENTE:
-          corSeta.direita = "preta";
-          break;
-      }
-    }
-    return (
-      <div className="seta-container">
-        <div className={"seta esquerda " + corSeta.esquerda}></div>
-        <div className={"seta direita " + corSeta.direita}></div>
-      </div>
-    );
-  }
-
   return (
     <div className="container">
       <div className="linha">
         <div className="celula primeira-coluna texto-esquerda borda celula-disciplina">
           <div className="seta-container"></div>
           <div className="nome-disciplina">{disciplina.nome}</div>
-          <div className="seta-container" onClick={mudarOrdem}>
-            {setasOrdenadoras()}
-          </div>
+          <SetasOrdenadoras
+            idElemento={disciplina.id}
+            ordenarCrescente={reducers.docentes.ordenarPorCompetenciaCrescente}
+            ordenarDecrescente={
+              reducers.docentes.ordenarPorCompetenciaDecrescente
+            }
+          />
         </div>
 
         <div className="celula azul-claro borda coluna-carga-horaria">
